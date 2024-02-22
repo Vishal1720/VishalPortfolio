ff2803462e0eaab92346eed54e6e87163f6b6861
Vishal <vishal198shetty@gmail.com>
2024-02-22 12:52:26 +0530

    preloader

--- loader.js
+++ loader.js
@@ -1,5 +1,8 @@
-document.addEventListener("DOMContentLoaded",function(){
-    var div1=document.getElementById("loader");
+var div1=document.getElementById("loader");
+window.addEventListener("load",function(){
+    
+    div1.style.zIndex="-1";
+    console.log(div1)
     div1.style.display="none";
-div1.style.zIndex="-1";
+
 });
